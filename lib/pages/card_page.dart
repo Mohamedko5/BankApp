1d5f35dd4a66436c3a93b7e207f647111e581c9f
Mohamedko5 <mohamedkofli2001@gmail.com>
2025-08-05 20:38:06 +0800

    Initial commit

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,3 +1,6 @@
+import 'package:bankapp/data_jons/card_json.dart' show cardLists;
+import 'package:bankapp/pages/dashboard_page.dart';
+import 'package:bankapp/theme/color.dart';
 import 'package:flutter/material.dart';
 
 class CardPage extends StatefulWidget {
@@ -11,51 +14,106 @@ class _CardPageState extends State<CardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Card Page')),
-      body: Center(
-        child: Card(
-          elevation: 4.0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                const Text(
-                  'This is a Card',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  'Cards are used to display content in a structured way.',
-                ),
-              ],
-            ),
-          ),
-        ),
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: getAppBar(context),
       ),
-      backgroundColor: Colors.grey[200],
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.home),
-              onPressed: () {
-                // Navigate to home
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.settings),
-              onPressed: () {
-                // Navigate to settings
-              },
-            ),
-          ],
-        ),
-      ),
+      body: getBody(context),
     );
   }
+}
+
+Widget getAppBar(BuildContext context) {
+  return AppBar(
+    elevation: 0,
+    backgroundColor: Colors.white,
+    leading: IconButton(
+      onPressed: () {
+        Navigator.pop(context);
+      },
+      icon: Icon(Icons.arrow_back_ios_new, color: black, size: 22),
+    ),
+    title: Text(
+      'Card',
+      style: TextStyle(color: black, fontSize: 20, fontWeight: FontWeight.w600),
+    ),
+    actions: [
+      IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.more_vert, color: black, size: 25),
+      ),
+    ],
+  );
+}
+
+Widget getBody(BuildContext context) {
+  var size = MediaQuery.of(context).size;
+  final PageController controller = PageController();
+  return Column(
+    children: [
+      SizedBox(height: 10),
+      Container(
+        width: double.infinity,
+        height: 240,
+        child: PageView(
+          controller: controller,
+          children: List.generate(cardLists.length, (index) {
+            return getCard(
+              cardLists[index]['amount'],
+              cardLists[index]['currency'],
+              cardLists[index]['card_number'],
+              cardLists[index]['valid_dateunt'],
+              cardLists[index]['bg_color'],
+              context,
+            );
+          }),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget getCard(
+  amount,
+  currency,
+  cardNumber,
+  validDate,
+  bgColor,
+  BuildContext context,
+) {
+  var size = MediaQuery.of(context).size;
+  return Column(
+    children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            currency,
+            style: TextStyle(
+              fontSize: 17,
+              color: black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(width: 5),
+          Text(
+            amount,
+            style: TextStyle(
+              fontSize: 17,
+              color: black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+      SizedBox(height: 15),
+      Container(
+        width: size.width * 0.85,
+        height: 170,
+        decoration: BoxDecoration(color: bgColor),
+      ),
+    ],
+  );
 }
